33aed503bdd7ac52f1a44a70fbd54720a5add538
JotaPeCreator <jotape.creator@outlook.com>
2025-08-20 20:36:14 -0500

    prueba1script

---
+++ script.js
@@ -0,0 +1,15 @@
+function validarFormulario(){
+    let nombre=document.getElementById("nombre").value;
+    let email=document.getElementById("email").value;
+    let imagen=document.getElementById("imagen").value;
+    if(nombre=="" || email=="" || imagen=="" ){
+        alert("Todos los cambios son obligatorios");
+        return false;
+    }else{
+        return true;
+    }
+}
+function mostrarImagen(event){
+    let imagen=document.getElementById('ver-imagen');
+    imagen.src=URL.createObjetURL(event.target.file[0]);
+}
